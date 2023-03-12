09d6393fceb42d03eb017a3c935be756cadad174
Corentin <88098360+Spiky-73@users.noreply.github.com>
2023-03-12 19:21:12 +0100

    Updated batch file

--- git/CommitPush.bat
+++ git/CommitPush.bat
@@ -1,7 +1,11 @@
 @echo off
 cd %~dp0
-set /p "message=Enter commit message: "
+echo Adding all changes
 git add -A
+echo Creating commit
+set /p "message=Enter commit message: "
 git commit -a -m "%message%"
+echo Pushing
 git push
+echo Pushed all changes to remote
 timeout /t -1
