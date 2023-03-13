5f49e6d1bca3358563fd1701dc3cb0fb7aa272de
Justine Gayot <jgayot@insa-lyon.fr>
2023-03-13 08:41:23 +0100

    test

--- BubbleShooter.py
+++ BubbleShooter.py
@@ -2,20 +2,20 @@
 Executez ce fichier pour jouer au jeu
 """
 
-import tkinter
+import tkinter as tk
 
 class BubbleShooter():
     
     def __init__(self):        
-        self.racine = tkinter.Tk()
+        self.racine = tk.Tk()
         self.racine.title("Fenêtre pour interactions Texte et Graphique...")
-        self.racine.config(width=300, height=700)
+        self.racine.config(width=400, height=700)
         self.racine.resizable(height = False, width = False)
 
-        self.canevas = tkinter.Canvas(self.racine, width=self.racine["width"], height=self.racine["height"])
+        self.canevas = tk.Canvas(self.racine, width=self.racine["width"], height=self.racine["height"])
         self.canevas.pack()
-
     
+
 if __name__ == "__main__":
     app = BubbleShooter()
     app.racine.mainloop()
