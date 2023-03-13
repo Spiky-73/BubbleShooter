c674971f3a6b8fb5561b86835c3a5f65ccd9ad26
Corentin <88098360+Spiky-73@users.noreply.github.com>
2023-03-13 08:22:30 +0100

    ajout d'un canvas

--- BubbleShooter.py
+++ BubbleShooter.py
@@ -9,7 +9,11 @@ class BubbleShooter():
     def __init__(self):        
         self.racine = tkinter.Tk()
         self.racine.title("Fenêtre pour interactions Texte et Graphique")
+        self.racine.config(width=300, height=700)
         self.racine.resizable(height = False, width = False)
+
+        self.canevas = tkinter.Canvas(self.racine, width=self.racine["width"], height=self.racine["height"])
+        self.canevas.pack()
 
     
 if __name__ == "__main__":
--- git/Pull.bat
+++ git/Pull.bat
@@ -1,4 +1,6 @@
 @echo off
+cd %~dp0
+echo Pulling all commits
 git pull
-echo Pulled all changes from remote
+echo Pulled all new commits from remote
 timeout /t -1
