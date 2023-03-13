8edbdc0a34061f29d615ac3a0abb6036d846a6a4
Solene Ferrer <sferrer@insa-lyon.fr>
2023-03-13 09:25:11 +0100

    début trajectoire

--- BubbleShooter.py
+++ BubbleShooter.py
@@ -15,6 +15,20 @@ class BubbleShooter():
         self.canevas = tk.Canvas(self.racine, width=self.racine["width"], height=self.racine["height"])
         self.canevas.pack()
     
+    def creer_widgets(self,root):
+        self.fen_graphique = None
+    
+    def trajectoire_balle(self,event):
+        
+        """ Trajectoire = droite entre la position de la souris au moment où on clique et la position du 
+        canon à billes en bas de l'écran. """
+        
+        mouseX = event.x # on récupère la position de la souris
+        mouseY = event.y
+        x = self.fen_graphique.canevas.canvasx(mouseX)
+        y = self.fen_graphique.canevas.canvasy(mouseY)
+        print(f"")
+        
 
 if __name__ == "__main__":
     app = BubbleShooter()
