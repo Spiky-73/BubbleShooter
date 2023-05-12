a4424bd45cc4de3862bec6486958aa0c884f3b50
clemenceard <clem.ardant@gmail.com>
2023-05-12 12:59:09 +0200

    clem

--- etats/finDePartie.py
+++ etats/finDePartie.py
@@ -12,7 +12,7 @@ class FinDePartie(Etat):
         """Initialise la fenêtre de fin de partie"""
         self.niveau= niveau
         if gagner==True:
-            message=f"Bravo ;) Tu as gagné ! \nTu as réussi à finir le jeu en {chrono} s\nTon score est de : {score}"
+            message=f"          Bravo !! Tu as gagné ;)\nTu as réussi à finir le jeu en {chrono} s\nTon score est de : {score}"
         elif gagner == False:
             message =f"Tu as perdu... \nTu as fini le jeu en {chrono} s\nTon score est de : {score}"
         
@@ -20,10 +20,10 @@ class FinDePartie(Etat):
         fenetre.grille.bind_tag("replay", self.replay)
         fenetre.grille.tag_bille(Vector2Int(3,22), "replay")
         fenetre.grille.bind_tag("menu", self.menu)
-        fenetre.grille.tag_bille(Vector2Int(20,22), "menu")
+        fenetre.grille.tag_bille(Vector2Int(19,22), "menu")
         self.ids = [
-            fenetre.canevas.create_text(*fenetre.grille.coordonees_to_position(Vector2Int(5,21)) - Vector2(fenetre.RAYON, 1), text="REJOUER", fill=theme.text[0], font="Helvetica 23 bold"),
-            fenetre.canevas.create_text(*fenetre.grille.coordonees_to_position(Vector2Int(20,21)) - Vector2(fenetre.RAYON, 1), text="MENU", fill=theme.text[0], font="Helvetica 23 bold"),
+            fenetre.canevas.create_text(*fenetre.grille.coordonees_to_position(Vector2Int(5,21)) - Vector2(fenetre.RAYON, 1), text="REJOUER", fill=theme.text[1], font="Helvetica 23 bold"),
+            fenetre.canevas.create_text(*fenetre.grille.coordonees_to_position(Vector2Int(19,21)) - Vector2(fenetre.RAYON, 1), text="MENU", fill=theme.text[0], font="Helvetica 23 bold"),
             fenetre.canevas.create_text(250,250, text=message, fill=theme.text[0], font="ComicSansMS 15 ")
             ]
         fenetre.grille.gelee = True
